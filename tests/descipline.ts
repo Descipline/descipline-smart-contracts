8416e3f5328366329e17653ebbc2d413bf353e73
Andrew Fang <imfangcong@gmail.com>
2025-07-29 10:42:55 +0000

    test: pass resolve test

--- tests/descipline.ts
+++ tests/descipline.ts
@@ -9,9 +9,15 @@ require("dotenv").config();
 // 1. credential_signer sign credential 
 // 2. sign schema
 // 3. create challenge
+
+// credential_authority: EmyHsCweqWZX7wPTKVmGxyEUJiP2mPXCi2rrBdgJvctW
+// initiator pubkey: 4k5ks1Tp6z4tUpJB5dqaW9HpuxrP9kqPCZMXLARWLuMv
+// loser pubkey: 4y3skg4uH6KBvumkXRSs6Dx3f6jFNDNSBmaT7Xo56aSU
+
 //  - Attestation PDA: CWvCj9CghVgkf8xGz6VXwrp2Vzz52b2DgYAUQ6vXhu4u
-const CREDENTIAL_PDA = new PublicKey("FZhMagtLNf7vnDN4hXee2jwXqgDjnN2Fx7CAJPkLr48g");
-const SCHEMA_PDA = new PublicKey("EqLuLrgNjzbQZhrEn563bwb6fv2hcxPo1jz4ZSXVH9Dq");
+const CREDENTIAL_PDA = new PublicKey("5C4smiRRFkPrMLrpT7yKzH6xpoJgEoGJhBQTRyi9qX1L");
+const SCHEMA_PDA = new PublicKey("5ku9sAQv1a8KgJ1gV9W5wTECdSoBDnjoaDc68957Xtsk");
+const Attestation_PDA = new PublicKey("9w19rmMYmrE2VNPJzfrQ86YcTrPp7H1oFPCEFqtQ5M6z");
 const USDC_MINT = new PublicKey("4NQMuSBhVrqTh8FMv5AbHvADVwHSnxrHNERPdAFu5B8p");
 
 describe("descipline", () => {
@@ -22,7 +28,7 @@ describe("descipline", () => {
 
   const program = anchor.workspace.descipline as Program<Descipline>;
   
-
+  console.log("programId is: ", program.programId)
   const credentialSigner = provider.wallet.payer;
   
   const credentialSignerPubkey = credentialSigner?.publicKey;
@@ -30,28 +36,32 @@ describe("descipline", () => {
   const authorizedSigners = [credentialSigner];
 
   // Decode the base58 private key string
-  const secretKeyBase58 = process.env.DEV_WAlLET_PRIVATE_KEY as string;
-  const secretKey = bs58.decode(secretKeyBase58);
-
-  // Create Keypair from secret key
+  let secretKeyBase58 = process.env.DEV_WAlLET_PRIVATE_KEY as string;
+  let secretKey = bs58.decode(secretKeyBase58);
   const initiator = Keypair.fromSecretKey(secretKey);
   console.log("initiator pubkey:", initiator.publicKey.toBase58());
   const credentialAuthorityPDA = anchor.web3.PublicKey.findProgramAddressSync([Buffer.from("authority")], program.programId)[0];
   console.log(credentialAuthorityPDA.toBase58())
 
-  const challenge_name = 'TEST-CHALLENGE';
-  const stake_amount = new anchor.BN(5000 * 10**6);
+  secretKeyBase58 = process.env.RUST_WALLET_PRIVATE_KEY as string;
+  secretKey = bs58.decode(secretKeyBase58);
+  const loser = Keypair.fromSecretKey(secretKey);
+  console.log("loser pubkey:", loser.publicKey.toBase58());
+
+  const challenge_name = 'TEST-CHALLENGE3';
+  const stake_amount = new anchor.BN(50 * 10**6);
   const fee = 100;
   const stake_end_at = new anchor.BN(Math.floor(Date.now() / 1000) +  15 * 3600); // seconds since epoch
   const claim_start_from = new anchor.BN(Math.floor(Date.now() / 1000) + 15 * 3600 + 1);
-
+  const initiator_is_winner = true;
   // Needed PDAs and mint
   let challengePda: PublicKey;
+  let resolutionPda: PublicKey;
   let vault: PublicKey;
   let stakeMint: PublicKey;
-  let receipt1;
-  let receipt2;
+  let receipt;
   let bump: number;
+  let tx: string;
 
   before(async () => {
     // derive challenge PDA
@@ -66,7 +76,7 @@ describe("descipline", () => {
   });
 
   it.skip("1. Initialize Credential Authority", async () => {
-    const tx = await program.methods
+    tx = await program.methods
     .initAuthority()
     .accountsPartial({
     })
@@ -82,7 +92,7 @@ describe("descipline", () => {
       true // allowOwnerOffCurve = true for PDA
     );
 
-    const tx = await program.methods
+    tx = await program.methods
     .createChallenge(
       challenge_name,
       {usdc: {}},
@@ -105,9 +115,9 @@ describe("descipline", () => {
     console.log("Your transaction signature", tx);
   });
 
-  it("3. Stake", async () => {
+  it.skip("3. Stake", async () => {
     // derive challenge PDA
-    [receipt1, bump] = await PublicKey.findProgramAddress(
+    [receipt, bump] = await PublicKey.findProgramAddress(
         [
           Buffer.from("receipt"),
           challengePda.toBuffer(),
@@ -115,13 +125,13 @@ describe("descipline", () => {
         ],
         program.programId
       );
-    const tx = await program.methods
+    tx = await program.methods
     .stake()
     .accountsPartial({
       challenger: initiator.publicKey,
       stakeMint: USDC_MINT,
       challenge: challengePda,
-      receipt: receipt1,
+      receipt: receipt,
       vault: vault
     })
     .signers([initiator])
@@ -129,7 +139,7 @@ describe("descipline", () => {
     console.log("Your transaction signature", tx);
 
     // derive challenge PDA
-    [receipt2, bump] = await PublicKey.findProgramAddress(
+    [receipt, bump] = await PublicKey.findProgramAddress(
       [
         Buffer.from("receipt"),
         challengePda.toBuffer(),
@@ -138,16 +148,65 @@ describe("descipline", () => {
       program.programId
     );
     
-    const tx1 = await program.methods
+    tx = await program.methods
     .stake()
     .accountsPartial({
       challenger: credentialSigner?.publicKey,
       stakeMint: USDC_MINT,
       challenge: challengePda,
-      receipt: receipt2,
+      receipt: receipt,
       vault
     })
     .rpc();    
-    console.log("Your transaction signature", tx1);
+    console.log("Your transaction signature", tx);
+
+    // loser join
+    [receipt, bump] = await PublicKey.findProgramAddress(
+      [
+        Buffer.from("receipt"),
+        challengePda.toBuffer(),
+        loser.publicKey.toBuffer(),
+      ],
+      program.programId
+    );
+    
+    tx = await program.methods
+    .stake()
+    .accountsPartial({
+      challenger: loser.publicKey,
+      stakeMint: USDC_MINT,
+      challenge: challengePda,
+      receipt: receipt,
+      vault
+    })
+    .signers([loser])
+    .rpc();    
+    console.log("Your transaction signature", tx);
+  });
+
+  it("4. Resolve", async () => {
+
+    // derive challenge PDA
+    [resolutionPda, bump] = await PublicKey.findProgramAddress(
+      [
+        Buffer.from("resolution"),
+        challengePda.toBuffer()
+      ],
+      program.programId
+    );
+
+
+    tx = await program.methods
+    .resolve()
+    .accountsPartial({
+      attestor: credentialSigner?.publicKey,
+      challenge: challengePda,
+      resolution: resolutionPda,
+      attestation: Attestation_PDA
+    })
+    .rpc();    
+    console.log("Your transaction signature", tx);
   })
+
+  
 });
